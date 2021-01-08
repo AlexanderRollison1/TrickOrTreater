50172cef6b6f40b8e5f97f4acb9896fd02d6fd13
Alexander Rollison <72281135+AlexanderRollison1@users.noreply.github.com>
2021-01-08 12:19:09 -0600

    Add files via upload

---
+++ House.cpp
@@ -0,0 +1,31 @@
+// House Class Template
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+
+#include "House.h"
+
+House::House(std::string Name, std::map<int,std::string>CandyRanking)
+	:houseName{Name}, CandyRankingMap{CandyRanking}
+{
+}
+
+std::string House::ringDoorbell(std::ostringstream &path)
+{
+	/* lock the door mutex */
+	door.lock();
+	path << "*";
+	/* put this_thread to sleep for 3 seconds */
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	//Pick a candy from the CandyRankingMap
+	int candy = rand() % (CandyRankingMap.size()) + 1;
+
+	/* unlock the door mutex */
+	door.unlock();
+	return CandyRankingMap[candy];
+}
---
+++ House.h
@@ -0,0 +1,33 @@
+// House class header file template Alexander Rollison 1001681800
+#ifndef _HOUSE_
+#define _HOUSE_
+
+/* add the necessary includes */
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+
+/* add an include guard named _HOUSE_  */
+
+class House
+{
+	public :
+		/* create the prototype for the constructor based on House.cpp */
+		/* create the prototype for ringDoorbell() based on House.cpp */
+		House(std::string Name, std::map<int,std::string>CandyRanking);
+		std::string ringDoorbell(std::ostringstream &path);
+		
+	private :
+		std::string houseName;
+		/* declare a mutex named door */
+		/* declare a map named CandyRankingMap of type int,string */
+		std::mutex door;
+		std::map<int, std::string> CandyRankingMap;
+};
+
+#endif
